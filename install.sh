94a6f2b407b1864a016181730f5cb11b2824550c
Toish <techpanda@digitalpanda.ca>
2015-01-27 19:47:26 -0500

    install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+build="../build/panda"
+if [ -f "/usr/local/bin/panda" ]; then
+	echo "Deleting old installed build"
+	sudo rm /usr/local/bin/panda
+fi
+echo "Moving latest build to /usr/local/bin"
+cp build/panda /usr/local/bin/panda
+echo "Done"
