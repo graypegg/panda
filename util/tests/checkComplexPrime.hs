ed222ce8db1dc967391697f5001daecfd5aaa335
Toish <techpanda@digitalpanda.ca>
2015-03-02 22:42:51 -0500

    better tests

--- util/tests/checkComplexPrime.hs
+++ util/tests/checkComplexPrime.hs
@@ -5,15 +5,37 @@ module Main where
 import System.Exit (exitFailure)
 import KeyGen
 import KeyTypes
+import KeyIO
 
 keyOneP = (keyGenComplex 2)
 keyOneS = 2
 keyTwoP = (keyGenComplex 3)
 keyTwoS = 3
 
+resultOne = commonKey keyOneP keyTwoS
+resultTwo = commonKey keyTwoP keyOneS
+
 main = do
-	if ((commonKey keyOneP keyTwoS)==(commonKey keyTwoP keyOneS)) then do
-		putStrLn "Prime passes simple DH exchange"
+	if (resultOne==resultTwo) then do
+		putStrLn "Prime passes complex DH exchange"
+		putStrLn "Alice:"
+		putStrLn ("    Public => " ++ (formatKey keyOneP))
+		putStrLn ("    Private => " ++ (formatSecret keyOneS))
+		putStrLn "Bob:"
+		putStrLn ("    Public => " ++ (formatKey keyTwoP))
+		putStrLn ("    Private => " ++ (formatSecret keyTwoS))
+		putStrLn "Result:"
+		putStrLn ("    " ++ (show resultOne))
 	else do
-		putStrLn "Prime fails simple DH exchange"
+		putStrLn "Prime fails complex DH exchange"
+		putStrLn "Alice:"
+		putStrLn ("    Public => " ++ (formatKey keyOneP))
+		putStrLn ("    Private => " ++ (formatSecret keyOneS))
+		putStrLn "Bob:"
+		putStrLn ("    Public => " ++ (formatKey keyTwoP))
+		putStrLn ("    Private => " ++ (formatSecret keyTwoS))
+		putStrLn "Result (Alice):"
+		putStrLn ("    " ++ (show resultOne))
+		putStrLn "Result (Bob):"
+		putStrLn ("    " ++ (show resultTwo))
 		exitFailure
--- util/tests/checkSimplePrime.hs
+++ util/tests/checkSimplePrime.hs
@@ -5,15 +5,37 @@ module Main where
 import System.Exit (exitFailure)
 import KeyGen
 import KeyTypes
+import KeyIO
 
 keyOneP = (keyGen 2)
 keyOneS = 2
 keyTwoP = (keyGen 3)
 keyTwoS = 3
 
+resultOne = commonKey keyOneP keyTwoS
+resultTwo = commonKey keyTwoP keyOneS
+
 main = do
-	if ((commonKey keyOneP keyTwoS)==(commonKey keyTwoP keyOneS)) then do
+	if (resultOne==resultTwo) then do
 		putStrLn "Prime passes simple DH exchange"
+		putStrLn "Alice:"
+		putStrLn ("    Public => " ++ (formatKey keyOneP))
+		putStrLn ("    Private => " ++ (formatSecret keyOneS))
+		putStrLn "Bob:"
+		putStrLn ("    Public => " ++ (formatKey keyTwoP))
+		putStrLn ("    Private => " ++ (formatSecret keyTwoS))
+		putStrLn "Result:"
+		putStrLn ("    " ++ (show resultOne))
 	else do
 		putStrLn "Prime fails simple DH exchange"
+		putStrLn "Alice:"
+		putStrLn ("    Public => " ++ (formatKey keyOneP))
+		putStrLn ("    Private => " ++ (formatSecret keyOneS))
+		putStrLn "Bob:"
+		putStrLn ("    Public => " ++ (formatKey keyTwoP))
+		putStrLn ("    Private => " ++ (formatSecret keyTwoS))
+		putStrLn "Result (Alice):"
+		putStrLn ("    " ++ (show resultOne))
+		putStrLn "Result (Bob):"
+		putStrLn ("    " ++ (show resultTwo))
 		exitFailure
